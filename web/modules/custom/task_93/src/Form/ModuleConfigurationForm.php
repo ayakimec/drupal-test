05c667fe32af13f218d3d7e0f5a8b9defea03310
ayakimec <andrei.works@yandex.ru>
2023-05-30 14:17:24 +0300

    added taxonomy country and condition

--- web/modules/custom/task_93/src/Form/ModuleConfigurationForm.php
+++ web/modules/custom/task_93/src/Form/ModuleConfigurationForm.php
@@ -4,6 +4,8 @@ namespace Drupal\task_93\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Defines a form that configures your_module’s settings.
@@ -26,11 +28,44 @@ class ModuleConfigurationForm extends ConfigFormBase {
     ];
   }
 
+
+    /**
+   * The entity service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface; $entityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a EntityTypeManagerInterface object
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface; $entity_type_manager
+   *   The module handler service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('task_93.admin_settings');
+    $vid = 'Country';
+    $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid);
+
+    foreach ($terms as $term) {
+      $countries[$term->name] = $term->name;
+    }
 
     $form['task_93'] = [
       '#type' => 'select',
@@ -38,11 +73,7 @@ class ModuleConfigurationForm extends ConfigFormBase {
       '#empty_option' => $this->t('--- Select Country ---'),
       '#empty_value' => '',
       '#default_value' => $config->get('task_93'),
-      '#options' => [
-        'Armenia' => $this->t('Armenia'),
-        'Belarus' => $this->t('Belarus'),
-        'United States' => $this->t('United States')
-      ],
+      '#options' => $countries
     ];
     return parent::buildForm($form, $form_state);
   }
