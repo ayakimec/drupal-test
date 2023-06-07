077416fe67f4393acfc8970f6eb13b6bcfb1be0a
andrey <petromath77@gmail.com>
2023-06-07 18:47:58 +0300

    added task-33

--- web/modules/custom/task_32/src/Form/CountryForm.php
+++ web/modules/custom/task_32/src/Form/CountryForm.php
@@ -4,10 +4,12 @@ namespace Drupal\task_32\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\TermInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\State\StateInterface;
 use Psr\Log\LoggerInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
 
 /**
  * Defines a form that configures your_module’s settings.
@@ -28,14 +30,6 @@ class CountryForm extends FormBase {
    */
   protected $entityTypeManager;
 
-
-  /**
-   * The state service.
-   *
-   * @var \Drupal\Core\State\StateInterface
-   */
-  protected $stateInterface;
-
   /**
    * A logger instance.
    *
@@ -48,14 +42,11 @@ class CountryForm extends FormBase {
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The module handler service.
-   * @param \Drupal\Core\State\StateInterface $state_interface
-   *   The module handler service.
    * @param \Psr\Log\LoggerInterface $logger
    *   A logger instance.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, StateInterface $state_interface, LoggerInterface $logger) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger) {
     $this->entityTypeManager = $entity_type_manager;
-    $this->stateInterface = $state_interface;
     $this->logger = $logger;
   }
 
@@ -65,7 +56,6 @@ class CountryForm extends FormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('state'),
       $container->get('logger.factory')->get('action'),
     );
   }
@@ -75,34 +65,43 @@ class CountryForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $vid = 'countries';
-    $cid = 'cities';
     $country = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid);
-    $city = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($cid);
 
     foreach ($country as $term) {
       $countries[$term->tid] = $term->name;
     }
 
-    foreach ($city as $term) {
-      $cities[$term->tid] = $term->name;
-    }
-
-    $form['country_name'] = [
+    $form['country'] = [
       '#type' => 'select',
       '#title' => $this->t('Select Country:'),
       '#empty_option' => $this->t('--- Select Country ---'),
       '#empty_value' => '',
-      '#default_value' => $this->stateInterface->get('country_name'),
       '#options' => $countries,
+      '#ajax' => [
+        'callback' => [$this, 'myAjaxCallback'],
+        'event' => 'change'
+      ]
     ];
 
-    $form['city_name'] = [
+    $cities_list = [];
+
+    if ($country_id = $form_state->getValue('country')) {
+      $cities = $this->entityTypeManager->getStorage('taxonomy_term')->loadByProperties(['field_country' => $country_id]);
+
+      foreach ($cities as $term) {
+        if ($term instanceof TermInterface) {
+          $cities_list[$term->id()] = $term->getName();
+        }
+      }
+    }
+
+    $form['city'] = [
       '#type' => 'select',
       '#title' => $this->t('Select City:'),
       '#empty_option' => $this->t('--- Select City ---'),
-      '#empty_value' => '',
-      '#default_value' => $this->stateInterface->get('city_name'),
-      '#options' => $cities,
+      '#options' => $cities_list,
+      '#prefix' => '<div id="city">',
+      '#suffix' => '</div>',
     ];
 
     $form['actions'] = [
@@ -119,16 +118,26 @@ class CountryForm extends FormBase {
   }
 
   /**
+   * Ajax callback function.
+   */
+  public function myAjaxCallback(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand("#city", ($form['city'])));
+
+    return $response;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->stateInterface->set('country_name', $form_state->getValue('country_name'));
-    $this->stateInterface->set('city_name', $form_state->getValue('city_name'));
+    $country_id = $form_state->getValue('country');
+    $city_id = $form_state->getValue('city');
 
-    $country_name = $this->stateInterface->get('country_name');
-    $city_name = $this->stateInterface->get('city_name');
+    $country = $this->entityTypeManager->getStorage('taxonomy_term')->load($country_id)->getName();
+    $city = $this->entityTypeManager->getStorage('taxonomy_term')->load($city_id)->getName();
 
-    $this->logger->info($country_name . '-' . $city_name);
+    $this->logger->info($country . '-' . $city);
 
     $this->messenger()->addMessage($this->t('The settings have been successfully saved'));
   }
